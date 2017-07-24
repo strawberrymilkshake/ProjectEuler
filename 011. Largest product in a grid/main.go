c4bbb8ad113bfd533a1db556832ea577981f04ef
Oleksii Troian <aleksej.trojan@gmail.com>
2017-07-25 01:01:46 +0300

    working on problem 11

--- 011. Largest product in a grid/main.go
+++ 011. Largest product in a grid/main.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"strconv"
 	"strings"
+	"sync"
 )
 
 //Error handling
@@ -33,7 +34,75 @@ func getnumslice(f string, x int, y int) [][]int {
 	return nums
 }
 
+var mut sync.Mutex
+
 func main() {
-	test := getnumslice("grid.txt", 20, 20)
-	fmt.Println(test)
+	var wg sync.WaitGroup
+
+	dat := getnumslice("grid.txt", 20, 20)
+	var product, maxproduct int
+
+	wg.Add(4)
+	//search down
+	go func() {
+		for i := 0; i <= 16; i++ {
+			for j := 0; j <= 19; j++ {
+				mut.Lock()
+				product = dat[i][j] * dat[i+1][j] * dat[i+2][j] * dat[i+3][j]
+				if product > maxproduct {
+					maxproduct = product
+				}
+				mut.Unlock()
+			}
+		}
+		wg.Done()
+	}()
+
+	//search right
+	go func() {
+		for i := 0; i <= 19; i++ {
+			for j := 0; j <= 16; j++ {
+				mut.Lock()
+				product = dat[i][j] * dat[i][j+1] * dat[i][j+2] * dat[i][j+3]
+				if product > maxproduct {
+					maxproduct = product
+				}
+				mut.Unlock()
+			}
+		}
+		wg.Done()
+	}()
+
+	//search diagonally down-right
+	go func() {
+		for i := 0; i < 17; i++ {
+			for j := 0; j < 17; j++ {
+				mut.Lock()
+				product = dat[i][j] * dat[i+1][j+1] * dat[i+2][j+2] * dat[i+3][j+3]
+				if product > maxproduct {
+					maxproduct = product
+				}
+				mut.Unlock()
+			}
+		}
+		wg.Done()
+	}()
+
+	//search diagonally down-right and up-right
+	go func() {
+		for i := 19; i >= 3; i-- {
+			for j := 0; j <= 16; j++ {
+				mut.Lock()
+				product = dat[i][j] * dat[i-1][j+1] * dat[i-2][j+2] * dat[i-3][j+3]
+				if product > maxproduct {
+					maxproduct = product
+				}
+				mut.Unlock()
+			}
+		}
+		wg.Done()
+	}()
+
+	wg.Wait()
+	fmt.Println(maxproduct)
 }
