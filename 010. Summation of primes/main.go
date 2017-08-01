b7b1c6f1f862d46eb11f12820f8765f1f513e42d
nic46 <nic46@nic46.pbank.cy>
2017-08-01 12:59:26 +0300

    after golint

--- 010. Summation of primes/main.go
+++ 010. Summation of primes/main.go
@@ -5,26 +5,26 @@ import (
 	"math"
 )
 
-// Only primes less than or equal to N will be generated
-const N = 1000000
+// Only primes less than or equal to limit will be generated
+const limit = 1000000
 
 func main() {
 	//https://en.wikipedia.org/wiki/Sieve_of_Atkin
 	var x, y, n, sum int
-	nsqrt := math.Sqrt(N)
-	primeness := [N]bool{}
+	nsqrt := math.Sqrt(limit)
+	primeness := [limit]bool{}
 	for x = 1; float64(x) <= nsqrt; x++ {
 		for y = 1; float64(y) <= nsqrt; y++ {
 			n = 4*(x*x) + y*y
-			if n <= N && (n%12 == 1 || n%12 == 5) {
+			if n <= limit && (n%12 == 1 || n%12 == 5) {
 				primeness[n] = !primeness[n]
 			}
 			n = 3*(x*x) + y*y
-			if n <= N && n%12 == 7 {
+			if n <= limit && n%12 == 7 {
 				primeness[n] = !primeness[n]
 			}
 			n = 3*(x*x) - y*y
-			if x > y && n <= N && n%12 == 11 {
+			if x > y && n <= limit && n%12 == 11 {
 				primeness[n] = !primeness[n]
 			}
 		}
@@ -32,7 +32,7 @@ func main() {
 
 	for n = 5; float64(n) <= nsqrt; n++ {
 		if primeness[n] {
-			for y = n * n; y < N; y += n * n {
+			for y = n * n; y < limit; y += n * n {
 				primeness[y] = false
 			}
 		}
@@ -48,7 +48,7 @@ func main() {
 		}
 	}
 
-	// primes is now a slice that contains all primes numbers up to N
+	// primes is now a slice that contains all primes numbers up to limit
 	// so let's print them
 	for _, x := range primes {
 		sum += x
