47a7e85b57b664d3d256da5f201fa2aafde24fd2
nic46 <nic46@nic46.pbank.cy>
2017-08-02 16:33:32 +0300

    Solution with math/big

--- 065. Convergents of e/main.go
+++ 065. Convergents of e/main.go
@@ -3,7 +3,31 @@ https://projecteuler.net/problem=65
 */
 package main
 
-//dummy
-func main() {
+import (
+	"fmt"
+	"math/big"
+	"strconv"
+)
 
+func mkfrac(i, limit int64) *big.Rat {
+	num := new(big.Rat)
+	if i%3 != 1 {
+		num.SetInt64(1)
+	} else {
+		num.SetInt64((i - 1) / 3 * 2)
+	}
+	if i >= limit {
+		return num
+	}
+	return num.Add(num, new(big.Rat).Inv(mkfrac(i+1, limit)))
+}
+
+func main() {
+	var sum int
+	r := mkfrac(0, 101).Num().String()
+	for i := 0; i < len(r); i++ {
+		digit, _ := strconv.Atoi(fmt.Sprintf("%c", r[i]))
+		sum += digit
+	}
+	fmt.Println(sum)
 }
