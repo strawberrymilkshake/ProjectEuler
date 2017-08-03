014df9d865b58573ebb3e0475608a16e92167432
nic46 <nic46@nic46.pbank.cy>
2017-08-03 18:06:54 +0300

    Dummy

---
+++ 243. Resilience/main.go
@@ -0,0 +1,14 @@
+// https://projecteuler.net/problem=243
+package main
+
+import (
+	"fmt"
+	"math/big"
+)
+
+//dummy
+func main() {
+	limit := new(big.Rat)
+	limit.SetFrac64(15499, 94744)
+	fmt.Println(limit)
+}
